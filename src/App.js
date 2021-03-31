7dbb1e3ac0dd60ff79712862981737b6010f338e
Emilse <emilsearias@gmail.com>
2021-03-30 22:38:14 -0300

    se agrego el componente frase con el maquetado para agregar datos del state del componente app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Button from 'react-bootstrap/Button'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 import Frase from "./components/Frase";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  // crear los state
+  const [personaje, setPersonaje] = useState({});
+
+  // ciclo de vida
+  useEffect(() => {
+    // aqui va la logica
+    consultaAPI();
+  }, []);
+
+  const consultaAPI = async() => {
+    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+    const result = await respuesta.json();
+    console.log(result[0]);
+    // guardar el resultado de mi consulta en el state
+    setPersonaje(result[0]);
+  };
+
   return (
-   <section className='container text-center my-5'>
-     <article className='d-flex flex-column align-items-center'>
-       <img src={process.env.PUBLIC_URL + 'logo.png'} alt="Logo de los simpsons" className='w-75 mb-5'/>
-       <Button variant="warning" className='w-50 mb-5'>Obtener Frase</Button>
-     </article>
-     <Frase></Frase>
-   </section>
+    <section className="container text-center my-5">
+      <article className="d-flex flex-column align-items-center">
+        <img
+          src={process.env.PUBLIC_URL + "logo.png"}
+          alt="Logo de los simpsons"
+          className="w-75 mb-5"
+        />
+        <Button variant="warning" className="w-50 mb-5" onClick={() => consultaAPI()}>
+          Obtener Frase
+        </Button>
+      </article>
+      <Frase propiedadPersonaje = {personaje}></Frase>
+    </section>
   );
 }
 
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-const Frase = () => {
+const Frase = (props) => {
   return (
     <Card>
       <Card.Body>
         <div className="row">
           <div className="col-sm-12 col-md-4">
-            <img src={''} alt="" />
+            <img src={props.propiedadPersonaje.image} alt={props.propiedadPersonaje.character} />
           </div>
           <div className="col-sm-12 col-md-8">
-            <Card.Title>Nombre de personaje</Card.Title>
-            <Card.Text>Frase aleatoria</Card.Text>
+            <Card.Title>{props.propiedadPersonaje.character}</Card.Title>
+            <Card.Text>{props.propiedadPersonaje.quote}</Card.Text>
           </div>
         </div>
       </Card.Body>
