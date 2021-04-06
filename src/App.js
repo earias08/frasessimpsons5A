d297a64b47964cae669f5b4e86a1a9e351974184
Emilse <emilsearias@gmail.com>
2021-04-06 10:06:34 -0300

    se agrego el componente spinner y su logica para mostrar el spinner o la frase

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Frase from "./components/Frase";
 import React, { useState, useEffect } from "react";
+import Spinner from "./components/Spinner";
 
 function App() {
   // crear los state
   const [personaje, setPersonaje] = useState({});
+  const [cargando, setCargando] = useState(false);
 
   // ciclo de vida
   useEffect(() => {
@@ -15,12 +17,22 @@ function App() {
   }, []);
 
   const consultaAPI = async() => {
+    // aqui quiero mostrar el spinner
+    setCargando(true);
     const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
     const result = await respuesta.json();
     console.log(result[0]);
-    // guardar el resultado de mi consulta en el state
-    setPersonaje(result[0]);
+    setTimeout(()=>{
+      // guardar el resultado de mi consulta en el state
+      setPersonaje(result[0]);
+      // quiero mostrar el componente frase
+      setCargando(false);
+    },3000)
   };
+
+  // operador ternario
+  // (condicion logica) ? (que pasa si la condicion se cumple):(que pasa si no se cumple la condicion)
+  const mostrarComponente = (cargando) ?  (<Spinner></Spinner>) : (<Frase propiedadPersonaje = {personaje}></Frase>)
 
   return (
     <section className="container text-center my-5">
@@ -34,7 +46,9 @@ function App() {
           Obtener Frase
         </Button>
       </article>
-      <Frase propiedadPersonaje = {personaje}></Frase>
+        {
+          mostrarComponente
+        }
     </section>
   );
 }
